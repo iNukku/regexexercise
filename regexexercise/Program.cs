90dc076157e1625d256a924c3a04baac28c0215b
Bertil Andersen <bertilandersen@hotmail.com>
2019-11-11 20:29:56 +0100

    Add project files.

---
+++ regexexercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+//Skriv et eller flere regulære udtræk, der er i stand til at finde danske mobil telefon numre, skrevet hhv. med og uden mellemrum, se ovenfor,  samt med eller uden angivelse af landekode, antaget nævnt i parentes. 
+
+namespace regexexercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"(\(\+?45\))?\s?(\d\s?-?){8}");
+            Regex regex2 = new Regex(@"(\(\+?45\))?(\s?\d{2}\s?-?){4}");
+
+            string searchstring = "My phoneNumber is 61 12 32 43. Sometimes i write (+45) 61 12 32 43 or even 61123243 or maybe 61-12-32-43";
+
+            foreach (Match match in regex.Matches(searchstring))
+            {
+                Console.WriteLine("Found the number: {0}. It was located at position: {1}", match.Value, match.Index);
+            }
+        }
+    }
+}
